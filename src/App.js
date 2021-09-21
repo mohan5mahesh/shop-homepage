2f253e3b309da631c1ed66bc476515460a0a92b1
Mohan <mohan5mahesh@gmail.com>
2021-09-21 15:34:47 +0530

    adding styles and button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import "./App.css";
+export default function App() {
+  const products = [
+    {
+      productname: "Fancy Product",
+      productprice: "$40.00 - $80.00",
+    },
+    {
+      productname: "Special Item",
+      productprice: " $18.00",
+    },
+    {
+      productname: "Sale Item",
+      productprice: " $25.00",
+    },
+    {
+      productname: "Popular Item",
+      productprice: "$40.00",
+    },
+    {
+      productname: "Sale Item",
+      productprice: " $25.00",
+    },
+    {
+      productname: "Fancy Product",
+      productprice: "$120.00 - $280.00",
+    },
+    {
+      productname: "Special Item",
+      productprice: " $18.00",
+    },
+    {
+      productname: "Popular Item",
+      productprice: "$40.00",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {products.map(({ productname, productprice }) => (
+        <Vote product={productname} price={productprice} />
+      ))}
     </div>
   );
 }
 
-export default App;
+function Vote({ product, price }) {
+  return (
+    <div className="card">
+      <div className="sizediv">450 x 300</div>
+      <div class="content">
+        <span className="sale">Sale</span>
+        <h2>{product}</h2>
+        <div className="rating"></div>
+        <p className="price">{price}</p>
+      </div>
+      <Cartbutton />
+    </div>
+  );
+}
+function Cartbutton() {
+  const [additem, setAdd] = useState(0);
+  return (
+    <div className="cartitems">
+      <div
+        className="changeitems"
+        style={{ display: additem === 0 ? "none" : "block" }}
+      >
+        <button onClick={() => setAdd(additem - 1)}>-</button>
+        <span className="additem">{additem} </span>
+        <button onClick={() => setAdd(additem + 1)}>+</button>
+        <br />
+      </div>
+
+      <div
+        className="zeroitems"
+        style={{ display: additem !== 0 ? "none" : "block" }}
+      >
+        <button className="add-to-cart" onClick={() => setAdd(additem + 1)}>
+          Add to cart
+        </button>
+      </div>
+    </div>
+  );
+}
